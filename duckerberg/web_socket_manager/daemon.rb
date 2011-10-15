fcb446f4d3265c8406439c0ba8c226a142c4e9bc
David Mah <mahhaha@gmail.com>
2011-10-15 06:51:28 -0700

    Using Daemons

--- duckerberg/web_socket_manager/daemon.rb
+++ duckerberg/web_socket_manager/daemon.rb
@@ -1,26 +1,3 @@
-require 'eventmachine'
-require 'em-websocket'
-load    'message_handler.rb'
+require 'daemons'
 
-handler = MessageHandler.new
-sockets = {}
-
-handler.log_message("Starting up Duckerberg server")
-
-EventMachine.run {
-
-  EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |socket|
-    socket.onopen {
-      handler.add_socket(socket)
-    }
-
-    socket.onmessage { |message|
-      handler.receive_message(message, socket)
-    }
-
-    socket.onclose {
-      handler.destroy_socket(socket)
-    }
-
-  end
-}
+Daemons.run('websocket_server.rb')
