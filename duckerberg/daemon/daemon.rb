49df39de8ea0b77cfee13c040a47372b1e29fcb9
David Mah <mahhaha@gmail.com>
2011-10-14 19:46:40 -0700

    Change Host Again

--- duckerberg/daemon/daemon.rb
+++ duckerberg/daemon/daemon.rb
@@ -2,7 +2,7 @@ require 'eventmachine'
 require 'em-websocket'
 
 EventMachine.run {
-  EventMachine::WebSocket.start(:host => "localhost", :port => 8080) do |socket|
+  EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |socket|
     socket.onopen {
       socket.send "Connection opened up bro"
       puts "Opened Connection"
