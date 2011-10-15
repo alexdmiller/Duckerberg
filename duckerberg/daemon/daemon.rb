867ea250d6b0cb2e4745befa98ee497595ba67eb
David Mah <mahhaha@gmail.com>
2011-10-14 19:27:16 -0700

    Logging for websocket to Standard Out

--- duckerberg/daemon/daemon.rb
+++ duckerberg/daemon/daemon.rb
@@ -5,10 +5,12 @@ EventMachine.run {
   EventMachine::WebSocket.start(:host => "127.0.0.1", :port => 8080) do |socket|
     socket.onopen {
       socket.send "Connection opened up bro"
+      puts "Opened Connection"
     }
 
     socket.onmessage { |msg|
       socket.send "huzzah got your: #{msg}"
+      puts "Received Message #{msg}"
     }
   end
 }
