4de1cd35c6e5424c06df2ac92df16a1db1eef1a2
David Mah <mahhaha@gmail.com>
2011-10-15 09:03:15 -0700

    Added Documentation and Cleanup to Backend
    - There is an Overall project README
    
    - Code is Commented and a little cleaner
    
    - Changed when information is transformed into JSON to encourage
        cleanliness

--- duckerberg/game_manager/daemon.rb
+++ duckerberg/game_manager/daemon.rb
@@ -1,3 +1,53 @@
-require 'daemons'
+# GameDaemon
+#
+# Receives and sends messages from the Web Socket Server through sets on redis
+# Offloads processing of the messages to GameManager
 
-Daemons.run('message_server.rb')
+require 'redis'
+require 'web_socket'
+load    'game_manager.rb'
+
+
+LOOP_RATE = 0.7
+
+class GameDaemon
+  def initialize
+    @game_manager  = GameManager.new
+    @outward_socket = WebSocket.new("ws://localhost:8080/")
+		@outward_socket.send "GAME_SERVER"
+    @redis          = Redis.new
+		@game_manager.log_message("Starting up Game Server")
+
+    # Monitoring Loop
+    loop {
+			start = Time.now.to_f
+
+      handle_inbox
+			@game_manager.prepare_game_signals
+      @outward_socket.send("READ")
+
+			margin = Time.now.to_f - start
+			sleep_time = (margin > LOOP_RATE ? LOOP_RATE : LOOP_RATE - margin)
+      sleep(sleep_time)
+    }
+  end
+
+  def handle_inbox
+    message = @redis.spop("inbox")
+    return if message.nil?
+    @redis.srem("inbox", message)
+    begin
+      message_hash            = JSON.parse(message)
+      @game_manager.handle_message(message_hash)
+    rescue
+      #@redis.sadd("inbox", message)
+      @game_manager.log_message("returned message to outbox :: #{message} :: #{$!} #{$!.backtrace.inspect}")
+    end
+  end
+
+  def pass_message(message)
+    @redis.sadd("outbox", message)
+  end
+end
+
+GameDaemon.new
