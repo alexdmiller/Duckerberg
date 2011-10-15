16db1981d0ade38e83782b0387fe8af87a0687c7
David Mah <mahhaha@gmail.com>
2011-10-14 21:03:37 -0700

    Logging added to received messages
    - /log/log.txt

--- duckerberg/daemon/daemon.rb
+++ duckerberg/daemon/daemon.rb
@@ -3,17 +3,19 @@ require 'em-websocket'
 load    'message_handler.rb'
 
 handler = MessageHandler.new
+sockets = {}
 
 EventMachine.run {
   EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |socket|
     socket.onopen {
       socket.send "Connection opened up bro"
-      puts "Opened Connection"
+      handler.log_message("Connection Opened")
+      add_socket(socket)
     }
 
     socket.onmessage { |message|
       socket.send "message received(#{message})"
-      handler.receive_message(msg)
+      handler.receive_message(message, socket)
     }
   end
 }
