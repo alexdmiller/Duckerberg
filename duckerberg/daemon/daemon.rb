b5a0f4a76ec5fadfa546c6387945b23c10e737a0
David Mah <mahhaha@gmail.com>
2011-10-14 19:27:16 -0700

    Basic WebSocket server set up, README has messages
    - We will be passing JSON back and forth

--- duckerberg/daemon/daemon.rb
+++ duckerberg/daemon/daemon.rb
@@ -2,13 +2,15 @@ require 'eventmachine'
 require 'em-websocket'
 
 EventMachine.run {
-  EventMachine::WebSocket.start(:host => "127.0.0.1", :port => 8080) do |socket|
+  EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |socket|
     socket.onopen {
       socket.send "Connection opened up bro"
+      puts "Opened Connection"
     }
 
     socket.onmessage { |msg|
       socket.send "huzzah got your: #{msg}"
+      puts "Received Message #{msg}"
     }
   end
 }
